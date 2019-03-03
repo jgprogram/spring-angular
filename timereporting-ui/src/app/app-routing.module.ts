c2f6fb7fa60db5cae56bddb453d451fd98244c45
Jacek Gzel <jacek.gzel@gmail.com>
2019-03-03 23:20:33 +0100

    Add base ui layout

--- timereporting-ui/src/app/app-routing.module.ts
+++ timereporting-ui/src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/workspace',
+    pathMatch: 'full'
+  },
+  {
     path: 'admin',
     loadChildren: './admin/admin.module#AdminModule'
   },
--- timereporting-ui/src/app/app.module.ts
+++ timereporting-ui/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule, MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +17,13 @@ import { AppComponent } from './app.component';
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
